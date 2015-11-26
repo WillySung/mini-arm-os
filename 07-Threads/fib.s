45bfa8b95f35e68b0c63cd93542199ef830b449d
WillySung <wanny820514@gmail.com>
2015-11-26 11:32:41 +0800

    the assembly code to compute fibonacci number

---
+++ 07-Threads/fib.s
@@ -0,0 +1,38 @@
+.syntax unified
+	.text
+	.align 2
+	.thumb
+	.thumb_func
+
+	.global fibonacci
+	.type fibonacci, %function
+
+fibonacci:
+
+	push {r3, r4, r5, lr}
+
+    subs r4, r0, #0             @ R4 = R0 - 0 (update flags)
+    ble .L3                     @ if(R0 <= 0) goto .L3 (which returns 0)
+
+    cmp r4, #1                  @ Compare R4 wtih 1
+    beq .L4                     @ If R4 == 1 goto .L4 (which returns 1)
+	
+    add r0, r4, #4294967295     @ R0 = R4 - 1
+    bl fibonacci                @ Recursive call to fibonacci with R4 - 1 as parameter
+	
+    mov r5, r0                  @ R5 = R0
+    sub r0, r4, #2              @ R0 = R4 - 2
+    bl fibonacci                @ Recursive call to fibonacci with R4 - 2 as parameter
+ 
+    add r0, r5, r0              @ R0 = R5 + R0 (update flags)
+
+	pop {r3, r4, r5, pc}		
+
+.L3:
+	mov r0, #0			        @ R0 = 0
+	pop {r3, r4, r5, pc}		
+
+.L4:
+	mov r0, #1			        @ R0 = 1
+	pop {r3, r4, r5, pc}		
+	
